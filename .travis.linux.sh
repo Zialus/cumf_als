950c603851f336e715153f6472caedb68ba3f537
Raul Ferreira <raulmendesferreira@gmail.com>
2018-06-12 01:41:40 +0100

    Big changes to travis scripts.

--- .travis.linux.sh
+++ .travis.linux.sh
@@ -4,16 +4,47 @@
 echo "Installing CMAKE"
 wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
 sudo sh cmake-3.11.3-Linux-x86_64.sh --prefix=/home/travis/.local/ --exclude-subdir
+
 ##### INSTALL CUDA #####
 echo "Installing CUDA library"
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
-sudo apt-get update -qq
-export CUDA_APT=${CUDA:0:3}
-export CUDA_APT=${CUDA_APT/./-}
-sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT} cuda-cublas-dev-${CUDA_APT} cuda-cusparse-dev-${CUDA_APT}
-sudo apt-get clean
-export CUDA_HOME=/usr/local/cuda-${CUDA:0:3}
-export LD_LIBRARY_PATH=${CUDA_HOME}/nvvm/lib64:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
-export PATH=${CUDA_HOME}/bin:${PATH}
+
+
+case ${CUDA:0:3} in
+
+'6.5')
+    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1404_7.0-28_amd64.deb
+    ;;
+'7.0')
+    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.0-28_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1404_7.0-28_amd64.deb
+    ;;
+'7.5')
+    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
+    ;;
+'8.0')
+    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+    ;;
+'9.0')
+    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+    travis_retry sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+    ;;
+'9.1')
+    travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
+    travis_retry sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+    ;;
+'9.2')
+    travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.2.88-1_amd64.deb
+    travis_retry sudo dpkg -i cuda-repo-ubuntu1604_9.2.88-1_amd64.deb
+    travis_retry sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+    ;;
+
+esac
+
+travis_retry sudo apt-get update
+travis_retry sudo apt-get install cuda -y
+travis_retry sudo apt-get clean
