9a99593addc1be08cf5e8df801c7f171e8b6f949
Raul Ferreira <raulmendesferreira@gmail.com>
2018-07-04 15:39:29 +0100

    Fixed bug in the tensorflow bash script.
    Cleaned up python part.

--- tensorflow/build_tf_op.sh
+++ tensorflow/build_tf_op.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #pre-requisite: CUDA_HOME set to cuda installation, e.g., /usr/local/cuda
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 nvcc -shared  -D_USE_GPU_ -I/usr/include ../als.cu ../host_utilities.cpp ../cg.cu -o libALS.so -Xcompiler "-fPIC" -m64  -use_fast_math -rdc=true -gencode arch=compute_35,code=sm_35 -gencode arch=compute_35,code=compute_35 -O3 -Xptxas -dlcm=ca -L{$CUDA_HOME}/lib64 -lcublas -lcusparse
 
-g++ -std=c++11 -shared als_tf.cc libALS.so -o als_tf.so -fPIC -I $TF_INC
+g++ -std=c++11 -shared als_tf.cc libALS.so -o als_tf.so -fPIC -I $TF_INC -L $TF_LIB -ltensorflow_framework
 
 
--- tensorflow/cumf_as_tensorflow_ops_test.py
+++ tensorflow/cumf_as_tensorflow_ops_test.py
@@ -3,6 +3,7 @@
 
 # In[1]:
 
+
 import tensorflow as tf
 lib_path = './als_tf.so'
 als_module = tf.load_op_library(lib_path)
@@ -10,24 +11,26 @@ als_module = tf.load_op_library(lib_path)
 
 # In[2]:
 
+
 import numpy as np
-#input data
-csrVal = np.fromfile('../data/netflix/R_train_csr.data.bin',dtype=np.float32)
-csrCol = np.fromfile('../data/netflix/R_train_csr.indices.bin',dtype=np.int32)
-csrRow = np.fromfile('../data/netflix/R_train_csr.indptr.bin',dtype=np.int32)
+# input data
+csrVal = np.fromfile('../data/netflix/R_train_csr.data.bin', dtype=np.float32)
+csrCol = np.fromfile('../data/netflix/R_train_csr.indices.bin', dtype=np.int32)
+csrRow = np.fromfile('../data/netflix/R_train_csr.indptr.bin', dtype=np.int32)
 
-cscVal = np.fromfile('../data/netflix/R_train_csc.data.bin',dtype=np.float32)
-cscRow = np.fromfile('../data/netflix/R_train_csc.indices.bin',dtype=np.int32)
-cscCol = np.fromfile('../data/netflix/R_train_csc.indptr.bin',dtype=np.int32)
+cscVal = np.fromfile('../data/netflix/R_train_csc.data.bin', dtype=np.float32)
+cscRow = np.fromfile('../data/netflix/R_train_csc.indices.bin', dtype=np.int32)
+cscCol = np.fromfile('../data/netflix/R_train_csc.indptr.bin', dtype=np.int32)
 
-cooRow = np.fromfile('../data/netflix/R_train_coo.row.bin',dtype=np.int32)
+cooRow = np.fromfile('../data/netflix/R_train_coo.row.bin', dtype=np.int32)
 
-cooRowTest = np.fromfile('../data/netflix/R_test_coo.row.bin',dtype=np.int32)
-cooColTest = np.fromfile('../data/netflix/R_test_coo.col.bin',dtype=np.int32)
-cooValTest = np.fromfile('../data/netflix/R_test_coo.data.bin',dtype=np.float32)
+cooRowTest = np.fromfile('../data/netflix/R_test_coo.row.bin', dtype=np.int32)
+cooColTest = np.fromfile('../data/netflix/R_test_coo.col.bin', dtype=np.int32)
+cooValTest = np.fromfile('../data/netflix/R_test_coo.data.bin', dtype=np.float32)
 
 
 # In[3]:
+
 
 m = 17770
 n = 480189
@@ -35,25 +38,20 @@ f = 100
 nnz = 99072112
 nnz_test = 1408395
 llambda = 0.048
-iters =10
+iters = 10
 xbatch = 1
 thetabatch = 3
 
 
 # In[4]:
 
+
 with tf.device('/cpu:0'):
     hello = tf.constant('Hello, TensorFlow!')
-    [thetaT,XT, rmse] = als_module.do_als(csrRow, csrCol, csrVal, cscRow, cscCol, cscVal, cooRow, 
-                                          cooRowTest, cooColTest, cooValTest, m, n, f, nnz, nnz_test, 
-                                          llambda, iters,xbatch, thetabatch, 0)
+    [thetaT, XT, rmse] = als_module.do_als(csrRow, csrCol, csrVal, cscRow, cscCol, cscVal, cooRow,
+                                           cooRowTest, cooColTest, cooValTest, m, n, f, nnz, nnz_test,
+                                           llambda, iters, xbatch, thetabatch, 0)
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 with sess:
-    print sess.run(hello)    
+    print sess.run(hello)
     print sess.run(rmse)
-
-
-# In[ ]:
-
-
-
