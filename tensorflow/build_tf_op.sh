687caf2829996d208100236a15800dee6d304cc6
Raul Ferreira <raulmendesferreira@gmail.com>
2019-02-10 13:59:41 +0000

    Improved build system.

--- tensorflow/build_tf_op.sh
+++ tensorflow/build_tf_op.sh
@@ -3,8 +3,10 @@
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
-nvcc -shared  -D_USE_GPU_ -I/usr/include ../als.cu ../host_utilities.cpp ../cg.cu -o libALS.so -Xcompiler "-fPIC" -m64  -use_fast_math -rdc=true -gencode arch=compute_35,code=sm_35 -gencode arch=compute_35,code=compute_35 -O3 -Xptxas -dlcm=ca -L{$CUDA_HOME}/lib64 -lcublas -lcusparse
+nvcc -shared  -D_USE_GPU_ -I/usr/include ../src/als.cu ../src/host_utilities.cpp ../src/cg.cu -o libALS.so \
+-Xcompiler "-fPIC" -m64 -use_fast_math -rdc=true -gencode arch=compute_35,code=sm_35 -gencode arch=compute_35,code=compute_35 \
+-O3 -Xptxas -dlcm=ca -L{$CUDA_HOME}/lib64 -lcublas -lcusparse
 
-g++ -std=c++11 -shared als_tf.cc libALS.so -o als_tf.so -fPIC -I $TF_INC -L $TF_LIB -ltensorflow_framework
+g++ -std=c++11 -shared als_tf.cc libALS.so -o als_tf.so -fPIC -I ${TF_INC} -L ${TF_LIB} -ltensorflow_framework
 
 
